3493daf7c3db0551a721d349b9a15053c2017350
kravolya <kravolya@ukr.net>
2020-01-17 13:07:39 +0200

    feferf

---
+++ src/js/burger.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const modalBtn = document.querySelector('.menu-btn-top');
+const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalWindow = document.querySelector('.modal-menu-fall');
+
+let openModalWindow = () => {
+    modalWindow.classList.add('modal-active');
+}
+
+let closeModalWindow = () => {
+    modalWindow.classList.remove('modal-active');
+}
+
+modalBtn.addEventListener('click', openModalWindow);
+modalBtnClose.addEventListener('click', closeModalWindow);
